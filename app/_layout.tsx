ac0708cc5e184b4af18d3a8d3e748f9a8236cb49
Jason Cabral <jpcabralofficial@gmail.com>
2024-11-11 23:59:55 +0800

    feat: initialized project setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+/** @format */
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import '../global.css';
+import '@/global.css';
+import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { FlowProvider } from '@/context/FlowContext';
+import 'react-native-get-random-values';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const queryClient = new QueryClient();
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+console.warn = () => '';
+console.error = () => '';
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -27,11 +35,23 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <GestureHandlerRootView className='flex-1'>
+      <QueryClientProvider client={queryClient}>
+        <FlowProvider>
+          <StatusBar
+            barStyle='dark-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          <Stack
+            screenOptions={{ headerShown: false }}
+            initialRouteName={'/(Onboarding)/index'}>
+            <Stack.Screen name={'/(Onboarding)/index'} />
+            <Stack.Screen name={'/JournalEntry'} />
+            <Stack.Screen name={'JournalEntry/CreateEntry'} />
+          </Stack>
+        </FlowProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
